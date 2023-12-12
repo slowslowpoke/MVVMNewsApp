5b99baf88a7cc429ebc3a6a2769393c0d18a46c1
Amina <amina.muradalieva91@gmail.com>
2023-12-12 12:58:06 +0600

    Added RecyclerView

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/adapters/NewsAdapter.kt
@@ -0,0 +1,65 @@
+package com.androiddevs.mvvmnewsapp.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.androiddevs.mvvmnewsapp.R
+import com.androiddevs.mvvmnewsapp.databinding.ItemArticlePreviewBinding
+import com.androiddevs.mvvmnewsapp.models.Article
+import com.bumptech.glide.Glide
+
+class NewsAdapter: RecyclerView.Adapter<NewsAdapter.ArticleViewHolder>() {
+
+    inner class ArticleViewHolder(val binding: ItemArticlePreviewBinding): RecyclerView.ViewHolder(binding.root){}
+
+    private val differCallback = object: DiffUtil.ItemCallback<Article>(){
+        override fun areItemsTheSame(oldItem: Article, newItem: Article): Boolean {
+            return oldItem.url == newItem.url
+        }
+
+        override fun areContentsTheSame(oldItem: Article, newItem: Article): Boolean {
+            return oldItem == newItem
+        }
+
+    }
+
+    val differ = AsyncListDiffer(this, differCallback)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArticleViewHolder {
+        /*return ArticleViewHolder(
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_article_preview, parent, false)
+        )*/
+        val binding = ItemArticlePreviewBinding.inflate(LayoutInflater.from(parent.context))
+        return ArticleViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return differ.currentList.size
+    }
+
+    override fun onBindViewHolder(holder: ArticleViewHolder, position: Int) {
+        val article = differ.currentList[position]
+
+        holder.binding.apply{
+            Glide.with(root).load(article.urlToImage).into(ivArticleImage)
+            tvSource.text = article.source.name
+            tvDescription.text = article.description
+            tvTitle.text = article.title
+            tvPublishedAt.text = article.publishedAt
+            root.setOnClickListener{
+                onItemClickListener?.let{ it(article) }
+            }
+        }
+
+    }
+
+    private var onItemClickListener: ((Article) -> Unit)? = null
+
+    fun setOnItemClickListener(listener: (Article) -> Unit) {
+        onItemClickListener = listener
+    }
+}
