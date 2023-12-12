9ef09a10ec265a79dee25018e35fcb86666335d8
Amina <amina.muradalieva91@gmail.com>
2023-12-12 10:03:54 +0600

    Added Retrofit Interface and Room DAO

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,9 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "d0999d42b9cc441db979be8638a05bb2"
+        const val BASE_URL = "https://newsapi.org"
+
+    }
+}
